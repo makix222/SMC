db72bb2884cd8b0dfd6507c50dbb91ab60c1e097
Emerson Maki <makix222@gmail.com>
2020-10-25 21:59:31 -0500

    Package Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SMC',
+    version='0.0.1',
+    packages=['smc'],
+    url='https://github.com/makix222/SMC',
+    license='MIT',
+    author='Emerson Maki',
+    author_email='makix222@gmail.com',
+    description=''
+)
