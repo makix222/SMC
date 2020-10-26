3eda8613d5188991da15e594a71843e93ce9d3bb
Emerson Maki <makix222@gmail.com>
2020-10-25 22:43:02 -0500

    Corrected for sg90 oddities

--- smc/main.py
+++ smc/main.py
@@ -1,9 +1,23 @@
-from gpiozero import AngularServo as angle_servo
+from gpiozero import Servo
 from time import sleep
 
-servo = angle_servo(14)
+servo_pin = 14
+sg90_correction = 0.45
+maxPW = (2.0 + sg90_correction)/1000
+minPW = (1.0 - sg90_correction)/1000
+
+servo = Servo(servo_pin, min_pulse_width=minPW, max_pulse_width=maxPW)
 
 while True:
-    angle = float(input("Angle?"))
-    servo.angle = angle
+    servo.mid()
+    print("mid")
+    sleep(0.5)
+    servo.min()
+    print("min")
+    sleep(1)
+    servo.mid()
+    print("mid")
+    sleep(0.5)
+    servo.max()
+    print("max")
     sleep(1)
